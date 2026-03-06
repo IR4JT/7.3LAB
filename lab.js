c3c5cca58c5570f9181a5598ed84c57bb61f33b5
IR4JT <tambatir@msu.edu>
2026-03-06 21:22:32 +0000

    code was wrong

--- lab.js
+++ lab.js
@@ -1,13 +1,18 @@
+
+import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js@2.7.1'
 const supabaseUrl = 'https://cudvmmrctalrvrgaysyi.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1ZHZtbXJjdGFscnZyZ2F5c3lpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzODQ4ODgsImV4cCI6MjA4Nzk2MDg4OH0.NzI09d3umKhIj4CURHA9dav4-qzqHZ3Q_Rt6nlhVDuw'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
+async function getBooks() {
+    let { data: Books, error } = await supabase
+        .from('Books')
+        .select('*')
 
-let { data: Books, error } = await supabase
-  .from('Books')
-  .select('*')
-  
-for (let book of Books) {
-    let bookList = document.getElementById('books');
-    bookList.innerHTML += `<li>${book.title}</li>`
+    for (let book of Books) {
+        let bookList = document.getElementById('books');
+        bookList.innerHTML += `<li>${book.title}</li>`;
+    }
 }
+
+getBooks();
